7106122c876a04a3ea206426d5ae44e959bd2a9b
jerryskye <jarek@siedlarz.com>
2017-10-21 00:21:35 +0200

    Migrating to Rails

--- resources/views/unauthorized.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="alert alert-danger">You are unauthorized to view this page.</div>
-    </div>
-</div>
-@endsection
