07a666a0779a037b5c9f26465d9350fdc5c83cc8
jerryskye <jarek@siedlarz.com>
2019-02-26 00:11:06 +0100

    Simplify the AddEntry service

--- app/services/add_entry.rb
+++ app/services/add_entry.rb
@@ -1,7 +1,7 @@
 class AddEntry < ApplicationService
   def call(uid, device_id)
-    lecture_id = Lecture.in_progress.find_by(device_id: device_id)&.id
-    entry = Entry.create(uid: uid, lecture_id: lecture_id, device_id: device_id)
+    lecture = Lecture.in_progress.find_by(device_id: device_id)
+    entry = Entry.create(uid: uid, lecture: lecture, device_id: device_id)
     if entry.persisted?
       Success(entry)
     else
